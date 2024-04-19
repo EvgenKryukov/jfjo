d3516f9548ad6ffbde237267b98cd6414d64342d
EvgenKryukov <161876861+EvgenKryukov@users.noreply.github.com>
2024-04-19 13:48:30 +0300

    Create 1.py

---
+++ 1.py
@@ -0,0 +1,58 @@
+(Варианты заданий A)
+import math
+
+x = 0.3
+y = 1
+
+result = math.pow(5, 2*x*y) - math.pow(x, 5*x) - math.exp(-math.pow(x, 2))
+print(result)
+
+
+import math
+
+x = 2
+y = 2
+
+result = math.sqrt(x + 4)**3 - (1/math.tan(x*y))/(3*y + 1)
+print(result)
+
+
+
+import math
+
+x = 1
+t = 3
+
+result = math.sqrt((math.cos(x)**2 + math.sin(x**2)) / (35*x*t))
+print(result)
+
+
+
+import math
+
+x = 3
+y = 3
+
+result = (abs(x) + 1) / 6 + (math.exp(-3*x) - 0.4) / (5 + 7*y)
+print(result)
+
+(Варианты заданий B)
+import math
+
+a = 0
+b = 1
+
+x = 2*a*b
+u = 5*a - 8*b
+
+result = math.log(x**2 + 1) - 4*u / (x**2 + 2)
+print(result)
+
+
+
+
+
+
+
+
+
